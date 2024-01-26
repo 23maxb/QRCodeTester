ccc77d6394e6ed21dc528f4d0e0527a6e57b6821
23maxb <80426654+23maxb@users.noreply.github.com>
2024-01-25 18:55:11 -0800

    Finished Project

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-if __name__ == '__main__':
+def makeQRCode(s):
     import segno
+    segno.make(s).save("basic_qrcode.png")
+
+
+def fileToQRCode():
+    import segno
+    os.listdir(path='.')
+    with open("geeks.txt") as file:
+        data = file.read()
+    segno.make(data).save("basic_qrcode.png")
+
+
+if __name__ == '__main__':
+    with open("ToQRCode/google.txt") as file:
+        data = file.read()
+    print(data)
     import re
-    pattern = re.compile("/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g")
+
+    pattern = re.compile(
+        "/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g")
     inp = input("Enter your value: ")
-    while not pattern.match(inp):
-        print("Invalid URL")
-        inp = input("Enter your value: ")
-    segno.make(inp).save("basic_qrcode.png")
+    if input == "files":
+        fileToQRCode()
+    else:
+        while not pattern.match(inp):
+            print("Invalid URL")
+            inp = input("Enter your value: ")
+        makeQRCode(input)
