df30b61dabdc0605a79eedaf4399417bba81ef4e
23maxb <80426654+23maxb@users.noreply.github.com>
2024-01-25 18:30:43 -0800

    Finished Project

---
+++ main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    import segno
+    import re
+    pattern = re.compile("/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g")
+    inp = input("Enter your value: ")
+    while not pattern.match(inp):
+        print("Invalid URL")
+        inp = input("Enter your value: ")
+    segno.make(inp).save("basic_qrcode.png")
